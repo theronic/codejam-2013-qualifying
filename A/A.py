df7b0c508f72ea7853eb2759d324ff99ffe75502
Petrus <github@petrus.co.za>
2013-04-14 19:38:56 +0200

    added .gitignore, README and removed debugging code from A/A.py

--- A/A.py
+++ A/A.py
@@ -2,83 +2,40 @@ ROWS = 4
 COLUMNS = 4
 T = -100
 
-IN_PROGRESS = -100
 X = 1
 O = -1
-NO_WIN = 0
-DRAW = 100
 
 def dump(board):
     print("\n".join(" ".join(map(str, line)) for line in board))
     print("\n")
 
-"""def diagonals(a): # via http://stackoverflow.com/a/10808134/198927
-    rows, cols = a.shape
-    if cols > rows:
-        a = a.T
-        rows, cols = a.shape
-    fill = numpy.zeros(((cols - 1), cols), dtype=a.dtype)
-    stacked = numpy.vstack((a, fill, a))
-    major_stride, minor_stride = stacked.strides
-    strides = major_stride, minor_stride * (cols + 1)
-    shape = (rows + cols - 1, cols)
-    return numpy.lib.stride_tricks.as_strided(stacked, shape, strides)
-"""
-
 def hasWon(acc, player):
     return acc * player == max(COLUMNS, ROWS)
 
 def testWin(board, player):
-    #dump(board)
     # test horizontal win
-    # test vertical win
     acc  = [ sum(row) for row in board ]
     if player*COLUMNS in acc:
         return True
-
-    #print("acc #1",acc)
 
     # test vertical win
     acc  = [ sum(x) for x in zip(*board) ]
     if player*ROWS in acc:
         return True
 
-    #print("acc #2",acc)
     # test diagonal win
     acc = board[0][0] + board[1][1] + board[2][2] + board[3][3]
     if acc == player * max(ROWS, COLUMNS):
         return True
-    #print(acc)
+
+    # test the other diagonal win
     acc1 = board[0][3] + board[1][2] + board[2][1] + board[3][0]
     if acc1 == player * max(ROWS, COLUMNS):
         return True
-    #print(acc1)
-    """acc, acc1 = 0, 0
-    for r in range(ROWS):
-        for c in range(COLUMNS):
-            print("row %d, column %d, index %d, rindex %d" % (r,c,r*ROWS+c,r*ROWS + COLUMNS - c - 1))
-            acc += board[r*ROWS + c]
-            acc1 += board[r*ROWS + COLUMNS - c - 1]
-    if hasWon(acc, player):
-        return True
-"""
-
 
     return False
 
 def solve(board, blocked):
-
-    """    Case #1: X won
-Case #2: Draw
-Case #3: Game has not completed
-Case #4: O won
-Case #5: O won
-Case #6: O won"""
-    # test horizontals
-    # test verticals
-    # test diagonals
-    acc = 0
-
     #test for an X win
     if blocked != (-1,-1):
         board[blocked[0]][blocked[1]] = X
@@ -128,7 +85,6 @@ for i in range(nCases):
             if val == -100:
                 blockedPos = (row,index)
             board[row][index] = parseChar(c)
-            #print(4*row + index, '=', board[4*row + index])
 
     print("Case #%d: %s" % (i+1, solve(board, blockedPos)))
 
@@ -136,7 +92,3 @@ for i in range(nCases):
         input() # read empty line
     except:
         pass
-
-
-    #a, b = map(int, input().split())
-    #print("Case #%d: %s" % (i+1, compute(fair, a, b)))
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-codejam-2013
-============
+# Google Code Jam 2013 Qualifying Round Solutions in Python 3
 
-Google Code Jam 2013 Solutions
+I wrote these solutions to the Google 2013 Qualifying Round after the contest. 
+Unfortunately, I could not compete during the contest.
+
+## Problem A
+
+## Problem B
+
+## Problem C
+
+## Problem D
